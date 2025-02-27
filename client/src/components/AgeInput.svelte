2cc4b1c73f7d2b1122efb48eaccdc52e6748912c
tirahimov <tirahimov@team.amocrm.com>
2025-02-27 13:01:50 +0300

    переименовал папку для сервера

---
+++ client/src/components/AgeInput.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  let {name,value} = $props();
+</script>
+
+<input type="number" name={name} bind:value required />
---
+++ client/src/components/NameInput.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  let { name, value } = $props();
+</script>
+
+<input type="text" {name} bind:value required />
